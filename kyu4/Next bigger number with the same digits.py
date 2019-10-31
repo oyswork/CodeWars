97a08247ad6f3fea555d4b22847e555c5f669808
oyswork <48206790+oyswork@users.noreply.github.com>
2019-10-31 17:02:28 +0200

    Andrey's approach, doesn't work correctly

--- kyu4/Next bigger number with the same digits.py
+++ kyu4/Next bigger number with the same digits.py
@@ -9,13 +9,34 @@ If no bigger number can be composed using those digits, return -1:
 111 ==> -1
 531 ==> -1 """
 
+# def next_bigger(n):
+#     if sorted([int(element) for element in str(n)])[::-1] != [int(element) for element in str(n)]: 
+#         karl = n+1
+#         while sorted([int(element) for element in str(karl)]) != sorted([int(element) for element in str(n)]):
+#             karl += 1
+#         return karl
+#     else: return -1
+
 def next_bigger(n):
-    if sorted([int(element) for element in str(n)])[::-1] != [int(element) for element in str(n)]: 
-        karl = n+1
-        while sorted([int(element) for element in str(karl)]) != sorted([int(element) for element in str(n)]):
-            karl += 1
-        return karl
-    else: return -1
+    digits = [int(d) for d in str(n)]
+    leading_zeros_passed = False
+    minDigitPosition = None
+    i = len(digits)
+    while i > 1:
+        i = i - 1
+        digit = digits[i]
+        nextDigit = digits[i-1]
+        if digit == 0 and not leading_zeros_passed:
+            continue
+        leading_zeros_passed = True
+        if minDigitPosition is None or digit < digits[minDigitPosition]:
+            minDigitPosition = i
+        if nextDigit < digit:
+            digits[i-1]=digits[minDigitPosition]
+            digits[minDigitPosition]=nextDigit
+            return int(''.join(map(str, digits)))
+
+    return -1
     
     print(karl)
 
