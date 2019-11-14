c1ef9d4bc3ffc479787e59107adbc8492daff7c8
oyswork <48206790+oyswork@users.noreply.github.com>
2019-11-14 18:09:33 +0200

    proper formating, solved

--- kyu4/nesting structure comparison.py
+++ kyu4/nesting structure comparison.py
@@ -1,23 +1,22 @@
-from collections.abc import Iterable
-def same_structure_as(original,other):
-    print(original,other)
-    #print(True not in [isinstance(element,list) for element in original])
-    if type(original)==type(other):
-        original_structure = [[str(type(element)),len(str(element))] for element in original]
-        other_structure = [[str(type(element)),len(str(element))] for element in other]
-        # print(original_structure,other_structure)
-        # print(original_structure==other_structure)
-        if original_structure==other_structure[::-1] and True not in [isinstance(element,list) for element in original]:
-            print(original_structure==other_structure[::-1])
-            return original_structure==other_structure[::-1]
+def same_structure_as(original, other):
+    print(original, other)
+    if type(original) == type(other):
+        original_structure = [[str(type(element)), len(
+            str(element))] for element in original]
+        other_structure = [[str(type(element)), len(str(element))]
+                           for element in other]
+        if original_structure == other_structure[::-1] and True not in [isinstance(element, list) for element in original]:
+            print(original_structure == other_structure[::-1])
+            return original_structure == other_structure[::-1]
         else:
-            print(original_structure==other_structure)
-            return original_structure==other_structure
+            print(original_structure == other_structure)
+            return original_structure == other_structure
     else:
         print('False')
         return False
 
-same_structure_as([1,'[',']'],['[',']',1])
+
+same_structure_as([1, '[', ']'], ['[', ']', 1])
 
 
-#https://www.codewars.com/kata/nesting-structure-comparison/train/python
+# https://www.codewars.com/kata/nesting-structure-comparison/train/python
