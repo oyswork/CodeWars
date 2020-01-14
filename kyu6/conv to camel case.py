d3b4c4a363c7a2cb9468f8fb8ac228a870047dcd
Artemii Stepankin <Artemii.Stepankin@flex.com>
2020-01-14 10:13:30 +0200

    somewhat ok solution

--- kyu6/conv to camel case.py
+++ kyu6/conv to camel case.py
@@ -1,8 +1,6 @@
-teststr = 'abc-def-ghk'
-karl = teststr.translate(teststr.maketrans('-_', '  ')).split()
-karl = ''.join([item.capitalize() if index > 0 else item for index,item in enumerate(karl)])
-print(karl)
+def to_camel_case(text):
+    cleared_list = text.translate(text.maketrans('-_', '  ')).split()
+    return ''.join([item.capitalize() if index > 0 else item for index,item in enumerate(cleared_list)])
 
+""" https://www.codewars.com/kata/517abf86da9663f1d2000003/solutions/python """
 
-# def DNAtoRNA(dna):
-#     return dna.replace('T', 'U')
